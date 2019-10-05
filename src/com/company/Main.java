35bf3a8e9fd89c90d093dcb174af6c1ef11ef73f
ANDREA123-opps <andrea.avila@ujcv.edu.hn>
2019-10-05 13:32:54 -0600

    Metodo burbuja implemeto de una clases que permite generar numeros aleartorios

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,36 +1,10 @@
 package com.company;
-import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
       //  ALGORITMO DE ORDENAMIENTO BURBUJA Y FUSION
-        int arreglo[] = new int[5];
-        int auxiliar = 0;
-
-        Scanner in = new Scanner(System.in);
-
-        //Introducir variables desde teclado
-
-        for (int i = 0; i < arreglo.length; i++){
-            System.out.println("Introduzca el valor de la posicion "+ i +" :");
-            arreglo[i] = in.nextInt();
-            System.out.println("");
-        }
-        //Aplicando el metodo de la burbuja
-        for (int i = 0; i < arreglo.length - 1; i++){
-            for (int j = i; j < arreglo.length; j++){
-                if (arreglo[i] > arreglo[j]){
-                    auxiliar = arreglo[i];
-                    arreglo[i] = arreglo[j];
-                    arreglo[j] = auxiliar;
-                }
-            }
-        }
-
-        //Mostrando los datos ordenados
-        for (int i = 0; i < arreglo.length; i++){
-            System.out.print(arreglo[i] + " ");
-        }
+      /* MetodoBurbuja bur = new MetodoBurbuja();
+       bur.crearArray();
+       bur.mostrar();*/
     }
 }
