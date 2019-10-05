b281ba4c59c0738c292f6ec2b9a9f6306b595c98
ANDREA123-opps <andrea.avila@ujcv.edu.hn>
2019-10-05 10:43:04 -0600

    Aplicando el metodo burbuja

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,27 @@
 package com.company;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
       //  ALGORITMO DE ORDENAMIENTO BURBUJA Y FUSION
+        int arreglo[] = new int[5];
+        int auxiliar = 0;
+
+        Scanner in = new Scanner(System.in);
+
+        //Introducir variables desde teclado
+
+        for (int i = 0; i < arreglo.length; i++){
+            System.out.println("Introduzca el valor de la posicion "+ i +" :");
+            arreglo[i] = in.nextInt();
+            System.out.println("");
+        }
+        //Aplicando el metodo de la burbuja
+        for (int i = 0; i < arreglo.length; i++){
+            for (int j = 0; i < arreglo.length; i++){
+
+            }
+        }
     }
 }
