696de00d3994fcca8210a228da0c371ed474ec81
ANDREA123-opps <andrea.avila@ujcv.edu.hn>
2019-10-05 11:02:01 -0600

    Metodo burbuja fuciona correctamente

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -18,10 +18,19 @@ public class Main {
             System.out.println("");
         }
         //Aplicando el metodo de la burbuja
-        for (int i = 0; i < arreglo.length; i++){
-            for (int j = 0; i < arreglo.length; i++){
-
+        for (int i = 0; i < arreglo.length - 1; i++){
+            for (int j = i; j < arreglo.length; j++){
+                if (arreglo[i] > arreglo[j]){
+                    auxiliar = arreglo[i];
+                    arreglo[i] = arreglo[j];
+                    arreglo[j] = auxiliar;
+                }
             }
+        }
+
+        //Mostrando los datos ordenados
+        for (int i = 0; i < arreglo.length; i++){
+            System.out.print(arreglo[i] + " ");
         }
     }
 }
