ff8ef777d13535dcee1d07e146983583ce61e916
ANDREA123-opps <andrea.avila@ujcv.edu.hn>
2019-10-15 17:22:11 -0600

    arreglo con 90 posicion

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -62,8 +62,8 @@ public class Main {
 
     public static void main(String[] args) {
       //  ALGORITMO DE ORDENAMIENTO BURBUJA Y FUSION
-        int[] arreglo1 = new int[80];
-        int[] arreglo2 = new int[80];
+        int[] arreglo1 = new int[90];
+        int[] arreglo2 = new int[90];
 
         for (int i = 0; i < arreglo1.length; i++){
             int exa = (int)(Math.random()*99)+1;
