1983e164ea042d40c1d4885f89b51212312038ff
ANDREA123-opps <andrea.avila@ujcv.edu.hn>
2019-10-15 17:59:12 -0600

    arreglo con 10000 posiciones

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -62,8 +62,8 @@ public class Main {
 
     public static void main(String[] args) {
       //  ALGORITMO DE ORDENAMIENTO BURBUJA Y FUSION
-        int[] arreglo1 = new int[1000];
-        int[] arreglo2 = new int[1000];
+        int[] arreglo1 = new int[10000];
+        int[] arreglo2 = new int[10000];
 
         for (int i = 0; i < arreglo1.length; i++){
             int exa = (int)(Math.random()*99)+1;
