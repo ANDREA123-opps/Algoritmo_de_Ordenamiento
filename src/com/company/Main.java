1dab0d83e9676f9141574dfbfbdde89b43c18850
ANDREA123-opps <andrea.avila@ujcv.edu.hn>
2019-11-11 20:41:43 -0600

    pequeños cambios

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,116 +1,95 @@
 package com.company;
-
-import java.util.PriorityQueue;
 import java.util.Random;
-import java.util.Timer;
-
 public class Main {
 
-    /*public static int[] bubbleSort(int[] arr){
+    public static void main(String[] args) {
+        // write your code here
+        int arr[] = new int[100000];
+        int arr2[] = new int[arr.length];
+        Random random = new Random();
+        for (int j = 0; j < arr.length; j++) {
+            arr[j] = random.nextInt();
+        }
+        System.arraycopy(arr,0,arr2,0,arr.length);
+        System.out.println("Array Before Bubble Sort");
+        for(int i=0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        Long t0 = System.nanoTime();
+        bubbleSort(arr);//sorting array elements using bubble sort
+        Long t1 = System.nanoTime();
+        System.out.println("Array After Bubble Sort");
+        for(int i=0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        System.out.println("El tiempo de ejecucuión del bubble sort es: "+ (( t1 - t0 )/1e-6)+" milisegundos");
+        System.out.println("////////////////////////////////////////////////////////////////////////////////////////");
 
+        ////////////////////////////////////////////////////////////////////////////////
+        System.out.println("Array Before Merge Sort");
+        for(int i=0; i < arr2.length; i++){
+            System.out.print(arr2[i] + " ");
+        }
+        System.out.println();
+        Long ti = System.nanoTime();
+        mergeSort(arr2,arr2.length);//sorting array elements using merge sort
+        Long tf = System.nanoTime();
+        System.out.println("Array After Merge Sort");
+        for(int i=0; i < arr2.length; i++){
+            System.out.print(arr2[i] + " ");
+        }
+        System.out.println();
+        System.out.println("El tiempo de ejecucuión del bubble sort es: "+ (( tf - ti )/1e-6)+" milisegundos");
+    }
+
+
+    public static int[] bubbleSort(int[] arr){
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr.length; j++) {
                 if(arr[i]< arr[j]){
-                    int exa = arr[i];
+                    int temp = arr[i];
                     arr[i] = arr[j];
-                    arr[j] = exa;
+                    arr[j] = temp;
                 }
             }
         }
         return arr;
-    }*/
-
-
-    public  static int[] mergeSort(int[] arr){
-
-        if(arr.length <= 1){
-            return arr;
-        }
-
-        int means = arr.length /2;
-
-        int[] lower = new int[means];
-        int[] higher = new int[arr.length - means];
-        for (int i = 0; i < means; i++) {
-            lower[i] = arr[i];
-        }
-
-        for (int i = 0; i < higher.length ; i++) {
-            higher[i] = arr[i + lower.length];
-        }
-        return  merge(mergeSort(lower), mergeSort(higher));
     }
-
-    public static int[] merge(int[] a, int[] b){
-        int[] retval = new int[a.length+ b.length];
-        int i = 0; int j = 0; int k = 0;
-        while (j < a.length && k < b.length ){
-            if(a[j] < b[k]){
-                retval[i++] = a[j++];
-            }else{
-                retval[i++] = b[k++];
+    public static void mergeSort(int[] a, int n) {
+        if (n < 2) {
+            return;
+        }
+        int mid = n / 2;
+        int[] l = new int[mid];
+        int[] r = new int[n - mid];
+        for (int i = 0; i < mid; i++) {
+            l[i] = a[i];
+        }
+        for (int i = mid; i < n; i++) {
+            r[i - mid] = a[i];
+        }
+        mergeSort(l, mid);
+        mergeSort(r, n - mid);
+        merge(a, l, r, mid, n - mid);
+    }
+    public static void merge(
+            int[] a, int[] l, int[] r, int left, int right) {
+        int i = 0, j = 0, k = 0;
+        while (i < left && j < right) {
+            if (l[i] <= r[j]) {
+                a[k++] = l[i++];
+            }
+            else {
+                a[k++] = r[j++];
             }
         }
-
-        while (j < a.length){
-            retval[i++] = a[j++];
+        while (i < left) {
+            a[k++] = l[i++];
         }
-
-        while (k < b.length){
-            retval[i++] = b[k++];
+        while (j < right) {
+            a[k++] = r[j++];
         }
-        return retval;
     }
-
-    public static void main(String[] args) {
-
-        // ALGORITMO HEAPSORT
-        long start = System.currentTimeMillis();
-        int []arr = {0};
-        PriorityQueue<Integer> queue = new PriorityQueue<>();
-        for (int anArr; arr){
-            queue.add(anArr);
-        }
-        for (int i = 0; !queue.isEmpty() ; i++) {
-            arr[i] = queue.remove(i);
-
-        }
-
-      //  System.out.println("Heapsort take" + (end - st));
-      //  ALGORITMO DE ORDENAMIENTO HeapSort Y MergeSort
-        int[] arreglo1 = new int[100000];
-        int[] arreglo2 = new int[100000];
-
-        for (int i = 0; i < arreglo2.length; i++){
-            int exa = (int)(Math.random()*99)+1;
-            arreglo1[i] = exa;
-            arreglo2[i] = exa;
-        }
-
-        arreglo1 = mergeSort(arreglo1);
-        arreglo2 = heapSort(arreglo2);
-
-        Timer timer = new Timer();
-
-        /*/ Algoritmo de ordenamiento burbuja
-        System.out.println("Tiempo de ejecucionn --- BUBBLE SORT---");
-        for (int i = 0; i < arreglo2.length; i++){
-            long Time = System.currentTimeMillis();
-            //System.out.print(arreglo2[i]+", ");
-            System.out.println(Time + " Nanosegundos");
-        }*/
-
-        // Algoritmo de ordenamiento por fusion
-        System.out.println("Tiempo de ejecucion de --- MERGE SORT ---");
-        for (int i = 0; i < arreglo1.length; i++) {
-            long Time = System.currentTimeMillis();
-          //  System.out.print(arreglo1[i]+", ");
-            System.out.println(Time + " Nanosegundos");
-            }
-
-
-
-    }
-
-    }
-
+}
